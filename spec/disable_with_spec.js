7a2cae660a311bfa6b858cd3ba9a64393a213a19
Josh Lubaway <josh.lubaway@gmail.com>
2011-07-28 20:31:47 -0700

    Moving tests around.

--- spec/disable_with_spec.js
+++ spec/disable_with_spec.js
@@ -1,5 +1,4 @@
 describe('jQuery.disableWith', function() {
-
   describe('getElements', function() {
     it('should grab elements with data- attributes', function() {
       var button = $('<input data-disable-with="foo">');
@@ -22,20 +21,6 @@ describe('jQuery.disableWith', function() {
     });
   });
 
-  describe('fn.disableWith', function() {
-    it('should disable a submit button', function() {
-      var button = $('<input type="submit">');
-      button.disableWith('foo');
-      expect(button.prop('disabled')).toBeTruthy('a message');
-    });
-
-    it('should set the value of the button', function() {
-      var button = $('<input type="submit">');
-      button.disableWith('foo');
-      expect(button.val()).toEqual('foo');
-    });
-  });
-
   describe('initialize', function() {
     it('should set up a click handler for valid elements', function() {
       var button = $('<input value="foo" data-disable-with="bar">');
@@ -52,5 +37,18 @@ describe('jQuery.disableWith', function() {
       button.remove();
     });
   });
+});
 
+describe('fn.disableWith', function() {
+  it('should disable a submit button', function() {
+    var button = $('<input type="submit">');
+    button.disableWith('foo');
+    expect(button.prop('disabled')).toBeTruthy('a message');
+  });
+
+  it('should set the value of the button', function() {
+    var button = $('<input type="submit">');
+    button.disableWith('foo');
+    expect(button.val()).toEqual('foo');
+  });
 });
